9a784a1c84d0a56a848585a2a5dcabd9ebcd15ec
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-09 21:11:34 +0530

    Project

--- LL/Adding.java
+++ LL/Adding.java
@@ -11,7 +11,7 @@ class Node {
     }
 }
 
-public class insert {
+public class Adding {
     public static void main(String[] args) {
         Node a=new Node(12);
         Node b=new Node(13);
---
+++ LL/addAtBeg.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class addAtBeg {
+
+}

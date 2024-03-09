a9891eecbf48ff129a6df3fe98e54b9f4171dae5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-09 21:13:17 +0530

    Project

--- LL/addAtBeg.java
+++ LL/addAtBeg.java
@@ -10,5 +10,12 @@ class Node {
 }
 
 public class addAtBeg {
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
 
+    }
 }

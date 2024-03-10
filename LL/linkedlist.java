9119eeecffe9fbcda37f84b943717fa19a513388
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-10 12:48:19 +0530

    Project

---
+++ LL/linkedlist.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class linkedlist {
+    public static class listing {
+        int data;
+        listing next;
+
+        listing(int data) {
+            this.data = data;
+        }
+
+    }
+
+    public static class list {
+        listing head = null;
+        listing tail = null;
+
+        void InsertatBeg(int data) {
+            listing newN = new listing(data);
+            if (head == null) {
+                head = tail = newN;
+            } else {
+                newN.next = head;
+                head = newN;
+            }
+        }
+
+        void display() {
+            listing temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        list ll = new list();
+        ll.InsertatBeg(1);
+        ll.InsertatBeg(12);
+        ll.InsertatBeg(15);
+        ll.display();
+    }
+}

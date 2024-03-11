d93ab92b602c6d81257cd97d13d09dd157dc60fd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-11 14:59:30 +0530

    Project

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,36 +1,47 @@
 package LinkedList;
+
 import java.util.Scanner;
+
 public class SinglyLinkedList {
-    public static class singly{
+    public static class singly {
         int data;
         singly next;
-        singly(int data){
-            this.data=data;
+
+        singly(int data) {
+            this.data = data;
         }
     }
-    public static class meathod{
-        singly head=null;
-        singly tail=null;
-        Scanner sc=new Scanner(System.in);
-        void create(int data){
-            singly newN=new singly(data);
-            newN.next=head;
-            head=newN;
+
+    public static class meathod {
+        singly head = null;
+        singly tail = null;
+        Scanner sc = new Scanner(System.in);
+
+        void create(int data) {
+            singly newN = new singly(data);
+            if (head == null) {
+                head = tail = newN;
+            } else {
+                tail.next = newN;
+                tail = newN;
+            }
         }
-        void display(){
-            singly cure=head;
-            while(cure!=null){
-                System.out.print(cure+" ");
-                cure=cure.next;
+
+        void display() {
+            singly cure = head;
+            while (cure != null) {
+                System.out.print(cure.data + " ");
+                cure = cure.next;
             }
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
-         meathod ob=new meathod();
-         ob.create(1);
-         ob.create(2);
-         ob.create(3);
-         ob.display();
+        meathod ob = new meathod();
+        ob.create(1);
+        ob.create(2);
+        ob.create(3);
+        ob.display();
     }
 }

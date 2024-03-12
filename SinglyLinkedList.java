b48c2124d866982958d9808b6281e7fec205f827
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-12 18:15:16 +0530

    Project

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -127,7 +127,7 @@ public class SinglyLinkedList {
                 temp=temp.next;
                 return;
             }
-            for(int i=1;i<index-1;i++){
+            for(int i=0;i<index-1;i++){
                 head=temp.next;
             }
             temp.next=temp.next.next;

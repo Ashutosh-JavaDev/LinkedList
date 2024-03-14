b7f0d3d5aa5aa3adee2634bc3c1b048691b4b636
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-14 21:38:35 +0530

    Project

--- LL/mergewoList.java
+++ LL/mergewoList.java
@@ -1,47 +1,49 @@
 import java.util.Scanner;
-class Nodes{
+
+class Nodes {
     int data;
     Nodes next;
-    Nodes(int data){
-        this.data=data;
+
+    Nodes(int data) {
+        this.data = data;
     }
-   public static class Linked{
-    Nodes head=null;
-    Nodes tail=null;
-    void insert(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Print Stop to stop inserting Value");
-        String val;
-        while(true){
-            val=sc.nextLine();
-            if(val.equalsIgnoreCase("Stop")){
-                break;
-            }
-            else{
-                try{
-                    int data=Integer.parseInt(val);
-                    Nodes newN=new Nodes(data);
-                    if(head==null){
-                        head=tail=newN;
+
+    public static class Linked {
+        Nodes head = null;
+        Nodes tail = null;
+
+        void insert() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Print Stop to stop inserting Value");
+            String val;
+            while (true) {
+                val = sc.nextLine();
+                if (val.equalsIgnoreCase("Stop")) {
+                    break;
+                } else {
+                    try {
+                        int data = Integer.parseInt(val);
+                        Nodes newN = new Nodes(data);
+                        if (head == null) {
+                            head = tail = newN;
+                        } else {
+                            Nodes temp = head;
+                            while (tail != null) {
+                                temp = temp.next;
+                            }
+                            tail.next = newN;
+                            tail = newN;
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid\nWrite Stop or Enter");
+                        val = sc.nextLine();
                     }
-                    else{
-                       Nodes temp=head;
-                       while(tail!=null){
-                        temp=temp.next;
-                       } 
-                       tail.next=newN;
-                       tail=newN;
-                    }
-                }
-                catch(NumberFormatException e){
-                    System.out.println("Invalid\nWrite Stop or Enter");
-                    val=sc.nextLine();
                 }
             }
         }
     }
-   }
 }
+
 public class mergewoList {
-    
+
 }

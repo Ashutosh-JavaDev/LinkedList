7495f32497592a29868ef0d7ae12ddf34ce60688
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-16 16:08:34 +0530

    Projects

--- BasicFormulas.java
+++ BasicFormulas.java
@@ -1,15 +1,23 @@
 package LinkedList;
+
 import java.util.Scanner;
-public class BasicFormulas{
-    public static class noding{
+
+public class BasicFormulas {
+    public static class noding {
         int data;
         noding next;
-        noding(int data){
-            this.data=data;
+
+        noding(int data) {
+            this.data = data;
         }
     }
-    public static class listing{
-        noding head=null;
-        noding tail=null;
+
+    public static class listing {
+        noding head = null;
+        noding tail = null;
+
+        void create(int data) {
+
+        }
     }
 }

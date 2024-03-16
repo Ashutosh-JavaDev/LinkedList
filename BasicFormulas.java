f25e4cbff3333459ee8ab8e0a5cac2848c260300
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-16 16:06:18 +0530

    Projects

---
+++ BasicFormulas.java
@@ -0,0 +1,11 @@
+package LinkedList;
+import java.util.Scanner;
+public class BasicFormulas{
+    class noding{
+        int data;
+        noding next;
+        noding(int data){
+            this.data=data;
+        }
+    }
+}

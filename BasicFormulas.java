33956fcc578f0fd2dd963277a5204d0ea4d82cef
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-18 23:07:11 +0530

    Projects

--- BasicFormulas.java
+++ BasicFormulas.java
@@ -142,47 +142,44 @@ public class BasicFormulas {
                     "Press 1: To Create Node  in Linked List\nPress 2: To Display\nPress 3: To Insert Element in Beggining\nPress 4: To Insert Element in Middle\nPress 5: To Insert Element at End\nPress 6: TO Delete ELement  From the Begonnong\nPress 7: To Delete Element from any of the Index\nPress 8:To Delete Element from the End\nPress 9: To Count the Size of the  Linked List\nPress 10: Exit");
             while (true) {
                 int press = sc.nextInt();
-
-                if (press < 11) {
-
-                    switch (press) {
-                        case 1:
-                            create();
-                            break;
-                        case 2:
-                            disp();
-                            break;
-                        case 3:
-                            System.out.println("Enter the Value want to add in Beggining");
-                            InsertatBeggining(sc.nextInt());
-                            break;
-                        case 4:
-                            System.out.println("Enter the Index and the Value want to Insert in that Index");
-                            InsertatMid(sc.nextInt(), sc.nextInt());
-                            break;
-                        case 5:
-                            System.out.println("Enter the Value want to add at End");
-                            InsertatEnd(sc.nextInt());
-                            break;
-                        case 6:
-                            deleteatBeggining();
-                            break;
-                        case 7:
-                            System.out.println("From which Index You want to delete Node");
-                            deleteAnyIndex(sc.nextInt());
-                            break;
-                        case 8:
-                            deleteAtend();
-                            break;
-                        case 9:
-                            counnt();
-                            break;
-                        case 10:
-                            break;
-                        default:
-                            System.out.println("Invlid Press");
-                            break;
-                    }
+                switch (press) {
+                    case 1:
+                        create();
+                        break;
+                    case 2:
+                        disp();
+                        break;
+                    case 3:
+                        System.out.println("Enter the Value want to add in Beggining");
+                        InsertatBeggining(sc.nextInt());
+                        break;
+                    case 4:
+                        System.out.println("Enter the Index and the Value want to Insert in that Index");
+                        InsertatMid(sc.nextInt(), sc.nextInt());
+                        break;
+                    case 5:
+                        System.out.println("Enter the Value want to add at End");
+                        InsertatEnd(sc.nextInt());
+                        break;
+                    case 6:
+                        deleteatBeggining();
+                        break;
+                    case 7:
+                        System.out.println("From which Index You want to delete Node");
+                        deleteAnyIndex(sc.nextInt());
+                        break;
+                    case 8:
+                        deleteAtend();
+                        break;
+                    case 9:
+                        counnt();
+                        break;
+                    case 10:
+                        System.out.println("Existing...");
+                        return;
+                    default:
+                        System.out.println("Invlid Press");
+                        break;
                 }
 
             }

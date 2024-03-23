c62b3933ae8a6bf27747a237b5e54605e4da436b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 14:12:24 +0530

    DataBase

--- LL/NthRemoveStart.java
+++ LL/NthRemoveStart.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
 public class NthRemoveStart {
-    
+    public static class nthStartRem{
+        int data;
+        nthStartRem next;
+        nthStartRem(int data){
+            this.data=data;
+        }
+    }
 }

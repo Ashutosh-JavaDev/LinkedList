a3a0db15dd396afeb1f2fff6ac7f81fd18e359b1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 14:28:27 +0530

    DataBase

--- LL/NthRemoveStart.java
+++ LL/NthRemoveStart.java
@@ -10,7 +10,7 @@ public class NthRemoveStart {
     public static class NthRemovingStart{
         public static void NthStartRem(nthStartRem head,int index){
             nthStartRem temp=head;
-            for(int i=0;i<index;i++){
+            for(int i=0;i<index-1;i++){
                 temp=temp.next;
             }
             temp.next=temp.next.next;

01ed81b7904f746f439edb8d461da46725fcb0b8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 11:13:06 +0530

    Linked List

---
+++ NthNodeFromstart.java
@@ -0,0 +1,13 @@
+package LinkedList;
+import java.util.Scanner;
+class nthNode{
+    int data;
+    nthNode next;
+    nthNode(int data){
+        this.data=data;
+        
+    }
+}
+public class NthNodeFromstart {
+    
+}

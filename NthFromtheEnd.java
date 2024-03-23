6f81f9beac5925017bd9d34aba6419c41147b15b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 11:57:28 +0530

    Linked List

--- NthFromtheEnd.java
+++ NthFromtheEnd.java
@@ -1,25 +1,41 @@
 package LinkedList;
+
 import java.util.Scanner;;
+
 public class NthFromtheEnd {
-    public static class nthEnd{
+    public static class nthEnd {
         int data;
         nthEnd next;
-        nthEnd(int data){
-            this.data=data;
+
+        nthEnd(int data) {
+            this.data = data;
         }
     }
-    public class nthEndNode{
-        public static void ending(nthEnd head,int index){
-            nthEnd fast=head;
-            nthEnd slow=head;
-            for(int i=0;i<index;i++){
-                fast=fast.next;
+
+    public static class nthEndNode {
+        public static void ending(nthEnd head, int index) {
+            nthEnd fast = head;
+            nthEnd slow = head;
+            for (int i = 0; i < index; i++) {
+                fast = fast.next;
             }
-            while(fast!=null){
-                slow=slow.next;
-                fast=fast.next;
+            while (fast != null) {
+                slow = slow.next;
+                fast = fast.next;
             }
             System.out.println(slow.data);
         }
+    }
+
+    public static void main(String[] args) {
+        nthEnd a = new nthEnd(1);
+        nthEnd b = new nthEnd(12);
+
+        nthEnd c = new nthEnd(13);
+
+        nthEnd d = new nthEnd(14);
+
+        nthEnd e = new nthEnd(15);
+
     }
 }

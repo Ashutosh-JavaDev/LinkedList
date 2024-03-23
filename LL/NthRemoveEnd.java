8c38987e06e4bbef40f3baf78ed8277f769b8c3e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 13:40:13 +0530

    DataBase

---
+++ LL/NthRemoveEnd.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class NthRemoveEnd {
+ public static class nthRemove{
+    int data;
+    nthRemove next;
+    nthRemove(int data){
+        this.data=data;
+    }
+ }    
+}

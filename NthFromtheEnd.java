40b69d03bd0148bb433fa71e495e6fe2421a7778
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 11:45:06 +0530

    Linked List

--- NthFromtheEnd.java
+++ NthFromtheEnd.java
@@ -1,5 +1,11 @@
 package LinkedList;
-
+import java.util.Scanner;;
 public class NthFromtheEnd {
-    
+    public static class nthEnd{
+        int data;
+        nthEnd next;
+        nthEnd(int data){
+            this.data=data;
+        }
+    }
 }

706c3552942816d27f768a0f9ecc23ee3328f590
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 11:26:06 +0530

    Linked List

--- NthNodeFromstart.java
+++ NthNodeFromstart.java
@@ -11,16 +11,31 @@ public class NthNodeFromstart {
             this.data = data;
         }
     }
-    public static class NthFind{
-        public static void finding(nthNode head,int index){
-            nthNode temp=head;
-           for(int i=0;i<=index;i++){
-                temp=temp.next;
+
+    public static class NthFind {
+        public static void finding(nthNode head, int index) {
+            nthNode temp = head;
+            for (int i = 0; i <= index; i++) {
+                temp = temp.next;
             }
             System.out.println(temp.data);
         }
     }
+
     public static void main(String[] args) {
-        
+        NthFind ob = new NthFind();
+        nthNode a = new nthNode(1);
+        nthNode b = new nthNode(2);
+
+        nthNode c = new nthNode(31);
+
+        nthNode d = new nthNode(14);
+
+        nthNode e = new nthNode(15);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        ob.finding(a, 2);
     }
 }

7a1d7d81137f6cbc2a0fa1232579f15b67ba54c4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-23 11:20:49 +0530

    Linked List

--- NthNodeFromstart.java
+++ NthNodeFromstart.java
@@ -11,5 +11,15 @@ public class NthNodeFromstart {
             this.data = data;
         }
     }
-
+    public static class NthFind{
+        public static void finding(nthNode head){
+            nthNode temp=head;
+            while(temp!=null){
+                temp=temp.next;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
 }

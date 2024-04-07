24c46fc59add0d3f49e27aff78d5c4bbc347eb9e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-07 17:53:18 +0530

    Linked Lkst

--- LL/AddatNth.java
+++ LL/AddatNth.java
@@ -14,39 +14,38 @@ public class AddatNth {
         public static void adding(addNth head, int position, int value) {
             addNth temp = new addNth(value);
             addNth item = head;
-            for (int i = 0; i <=position; i++) {
+            for (int i = 0; i <= position; i++) {
                 item = item.next;
             }
-            temp.next=item.next.next;
-            item.next=temp;
+            temp.next = item.next.next;
+            item.next = temp;
         }
-        public static void display(addNth head){
-            addNth temp=head;
-            while(temp!=null){
-                System.out.print(temp.data+" ");
-                temp=temp.next;
+
+        public static void display(addNth head) {
+            addNth temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
             }
             System.out.println();
 
         }
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        addNth a=new addNth(1);
-        addNth b=new addNth(2);
-        addNth c=new addNth(4);
-        addNth d=new addNth(5);
-        a.next=b;
-        b.next=c;
-        c.next=d;
-        nthAdd ob=new nthAdd();
+        Scanner sc = new Scanner(System.in);
+        addNth a = new addNth(1);
+        addNth b = new addNth(2);
+        addNth c = new addNth(4);
+        addNth d = new addNth(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        nthAdd ob = new nthAdd();
         System.out.println("Enter the Position where you want ot add Data");
-        int position=sc.nextInt();
+        int position = sc.nextInt();
         ob.adding(a, position, 3);
-
-
-
-
+        ob.display(a);
 
     }
 }

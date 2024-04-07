688efc5b0e3484027fd96651bb2d4e1208ee495e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-07 17:52:55 +0530

    Linked Lkst

--- LL/AddatNth.java
+++ LL/AddatNth.java
@@ -23,9 +23,11 @@ public class AddatNth {
         public static void display(addNth head){
             addNth temp=head;
             while(temp!=null){
+                System.out.print(temp.data+" ");
                 temp=temp.next;
             }
-            
+            System.out.println();
+
         }
     }
     public static void main(String[] args) {

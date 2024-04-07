e0a17920ca3d16f28f229f44633e89786d6b01c9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-07 17:29:25 +0530

    Linked Lkst

--- LL/AddatNth.java
+++ LL/AddatNth.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
-class addNth{
-    int data;
-    addNth value;
-    
-}
+
 public class AddatNth {
-    
+    public static class addNth{
+        int data;
+        addNth node;
+        addNth (int data){
+            this.data=data;
+        }
+    }
+    public static class nthAdd{
+        public void adding(addNth head){
+            
+        }
+    }
 }

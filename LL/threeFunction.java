3cab377098d22ddaaf8d8613b637f30ff6275f5f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-08 14:21:01 +0530

    Linked Lkst

---
+++ LL/threeFunction.java
@@ -0,0 +1,21 @@
+// Insertion, Delete and Transveral
+import java.util.Scanner;
+public class threeFunction {
+    public static class Three {
+        int data;
+        Three next;
+
+        Three(int data) {
+            this.data = data;
+        }
+    }
+    public static class functions{
+        Three head;
+        Three tail;
+        public static void create(){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Press stop to Stop inserting Value");
+            System.out.println("Enter Value in the List: ");
+        }
+    }
+}

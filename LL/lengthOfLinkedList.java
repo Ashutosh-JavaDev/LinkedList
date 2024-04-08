eef05f4754a7b8a00f8c7ef12904ab3c58f579ac
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-08 12:13:59 +0530

    Linked Lkst

--- LL/lengthOfLinkedList.java
+++ LL/lengthOfLinkedList.java
@@ -1,4 +1,20 @@
 import java.util.Scanner;
+class length{
+    int data;
+    length next;
+    length (int value){
+        this.data=data;
+    }   
+    public static void disp(length head){
+        length temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    } 
+}
+
 public class lengthOfLinkedList {
     
 }

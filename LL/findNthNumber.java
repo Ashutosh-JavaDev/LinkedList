4a02791e0e39e0f7bf3adbfd6389ad7c3b7a2bda
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-08 13:02:20 +0530

    Linked Lkst

---
+++ LL/findNthNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class findNthNumber {
+    public static class NthNumber{
+        int data;
+        NthNumber next;
+        NthNumber(int value){
+            this.data=value;
+        }
+    }    
+}

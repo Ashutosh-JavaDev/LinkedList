c181cf861c92c10540463176ec9fa8d89e2cb4ea
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-08 13:43:00 +0530

    Linked Lkst

---
+++ LL/Reverse.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Reverse {
+    public static class rev{
+        int data;
+        rev next;
+        rev(int data){
+            this.data=data;
+        }
+    }    
+}

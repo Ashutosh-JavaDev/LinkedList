7a251340261a6404f7df54903464eccec48c3c37
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-09 17:42:04 +0530

    Linked List

--- LL/deleteFromValue.java
+++ LL/deleteFromValue.java
@@ -1,5 +1,9 @@
 public class deleteFromValue {
     public static class itemNode{
-        
+        int data;
+        itemNode next;
+        itemNode(int data){
+            this.data=data;
+        }
     }
 }

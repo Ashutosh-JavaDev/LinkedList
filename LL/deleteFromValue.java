5109a141614a947b7fd666118fff132f551fe864
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-09 17:59:59 +0530

    Linked List

--- LL/deleteFromValue.java
+++ LL/deleteFromValue.java
@@ -9,33 +9,44 @@ public class deleteFromValue {
     }
 
     public static class deleteValue {
-        public static void askDelete(itemNode head,int value) {
-            itemNode temp=head;
-            int c=0;
-            while(temp!=null){
-                if(temp.next.data==value){
+        public static void askDelete(itemNode head, int value) {
+            itemNode temp = head;
+            int c = 0;
+            while (temp != null) {
+                if (temp.next.data == value) {
                     c++;
-                    temp.next=temp.next.next;
+                    temp.next = temp.next.next;
+                    return;
                 }
-                temp=temp.next;
+                temp = temp.next;
             }
-            if(c==0){
+            if (c == 0) {
                 System.out.println("Value is Not in the List for Delete");
             }
 
         }
+
+        public static void disp(itemNode head) {
+            itemNode temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+
+                temp = temp.next;
+            }
+        }
     }
+
     public static void main(String[] args) {
-        itemNode a=new itemNode(1);
-        itemNode b=new itemNode(2);
-        itemNode c=new itemNode(3);
-        itemNode d=new itemNode(4);
-        itemNode e=new itemNode(5);
-        a.next=b;
-        b.next=c;
-        c.next=d;
-        d.next=e;
-        deleteValue ob=new deleteValue();
-        ob.askDelete(a, 3);
+        itemNode a = new itemNode(1);
+        itemNode b = new itemNode(2);
+        itemNode c = new itemNode(3);
+        itemNode d = new itemNode(4);
+        itemNode e = new itemNode(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        deleteValue ob = new deleteValue();
+        ob.askDelete(a, 2);
     }
 }

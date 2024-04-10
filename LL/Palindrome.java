d89728dba9cd7f8583558bb424675bb4e01b6e53
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-10 13:22:14 +0530

    Linked List

--- LL/Palindrome.java
+++ LL/Palindrome.java
@@ -6,4 +6,14 @@ public class Palindrome {
             this.data=data;
         }
     }
+    public static class drome{
+        public static void disp(palin head){
+            palin temp=head;
+            if(temp==null){
+                return;
+            }
+            System.out.print(temp.data+" ");
+            disp(temp.next);
+        }
+    }
 }

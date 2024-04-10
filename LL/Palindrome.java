fe7f2d684aa7b675cadfec19c118264efe79ec7e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-10 15:17:37 +0530

    Linked List

--- LL/Palindrome.java
+++ LL/Palindrome.java
@@ -1,27 +1,40 @@
 public class Palindrome {
-    public static class palin{
+    public static class palin {
         int data;
         palin next;
-        palin(int data){
-            this.data=data;
+
+        palin(int data) {
+            this.data = data;
         }
     }
-    public static class drome{
-        public static void rem(palin head){
-            palin temp=head;
-            String st=" ";
-            while(temp!=null){
-                int d=temp.data;
-                 st=d+" ";
-                temp=temp.next;
+
+    public static class drome {
+        public static void rem(palin head) {
+            palin temp = head;
+            String st = " ";
+            String str = " ";
+            while (temp != null) {
+                int d = temp.data;
+                st = d + " ";
+                temp = temp.next;
             }
-        } 
-        public static void disp(palin head){
-            palin temp=head;
-            if(temp==null){
+            for (int i = st.length(); i >= 0; i--) {
+                char ch = st.charAt(i);
+                str = ch + " ";
+            }
+            if (st.equals(str)) {
+                System.out.println("List is Palindrome");
+            } else {
+                System.out.println("List is not palindrome");
+            }
+        }
+
+        public static void disp(palin head) {
+            palin temp = head;
+            if (temp == null) {
                 return;
             }
-            System.out.print(temp.data+" ");
+            System.out.print(temp.data + " ");
             disp(temp.next);
         }
     }

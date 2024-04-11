cb7cf177913a4f3be49c1d6006452506bcfee425
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-11 09:23:17 +0530

    Linked List

---
+++ LL/MergeSortedLL.java
@@ -0,0 +1,10 @@
+public class MergeSortedLL {
+    public static class merge {
+        int data;
+        merge next;
+
+        merge(int data) {
+            this.data = data;
+        }
+    }
+}

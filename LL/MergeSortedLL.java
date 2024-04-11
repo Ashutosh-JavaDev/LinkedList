b1a53d03d369e06bb235fcb3a1373e3a3a2a9b27
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-11 11:00:09 +0530

    Linked List

--- LL/MergeSortedLL.java
+++ LL/MergeSortedLL.java
@@ -28,4 +28,31 @@ public class MergeSortedLL {
         }
         return dummy.next;
     }
+
+    public static void disp(merge head) {
+        merge temp = head;
+        if (temp == null) {
+            return;
+        }
+        System.out.print(temp.data + " ");
+        disp(temp.next);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        merge l1 = new merge(1);
+        l1.next = new merge(3);
+        l1.next.next = new merge(5);
+        merge l2 = new merge(2);
+        l2.next = new merge(4);
+        l2.next.next = new merge(6);
+        System.out.println("List 1:");
+        disp(l1);
+        System.out.println("List 2:");
+        disp(l2);
+        merge sorted = sorted(l1, l2);
+        System.out.println("Merge List");
+        disp(sorted);
+
+    }
 }

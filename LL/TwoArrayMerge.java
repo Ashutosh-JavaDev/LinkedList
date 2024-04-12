1d445e58c1295749737b5fda31dcdee146f2ef3a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-12 13:39:02 +0530

    Linked List

--- LL/TwoArrayMerge.java
+++ LL/TwoArrayMerge.java
@@ -1,7 +1,8 @@
 public class TwoArrayMerge {
     int data;
     TwoArrayMerge next;
-    TwoArrayMerge (int data){
-        this.data=data;
+
+    TwoArrayMerge(int data) {
+        this.data = data;
     }
 }

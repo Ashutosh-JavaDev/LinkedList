57c8f2139686c5fb4b169e648f3dccd92f19af6b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-05 14:34:04 +0530

    Linked List DSA

---
+++ LL/allFunction.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class allFunction {
+    class makeNode {
+        int data;
+        makeNode next;
+
+        makeNode(int data) {
+            this.data = data;
+        }
+    }
+}

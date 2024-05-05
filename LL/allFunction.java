3e9b95ae94cc39739b1379afbb985ef8810a8ebd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-05 14:39:05 +0530

    Linked List DSA

--- LL/allFunction.java
+++ LL/allFunction.java
@@ -1,12 +1,28 @@
 import java.util.Scanner;
 
 public class allFunction {
-    class makeNode {
+    public static class makeNode {
         int data;
         makeNode next;
 
         makeNode(int data) {
             this.data = data;
+        }
+    }
+
+    public static class Linked {
+        makeNode head = null;
+        makeNode tail = null;
+
+        void disp() {
+            makeNode temp = head;
+            while (temp.next != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+        void create(int data){
+            
         }
     }
 }

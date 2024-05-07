b4a9c010f062f92555a5365c10dcfa09732203a1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-07 21:29:25 +0530

    Linked List

--- LL/allFunction.java
+++ LL/allFunction.java
@@ -57,10 +57,12 @@ public class allFunction {
                 head = newNode;
             }
         }
-        static void addAtEnd(int data){
-            makeNode newNode=new makeNode(data);
-            if(head==null){
-                head=tail=newNode;
+
+        static void addAtEnd(int data) {
+            makeNode newNode = new makeNode(data);
+            if (head == null) {
+                head = tail = newNode;
+                return;
             }
         }
     }

97cdf83fa9ffc7e1433d7288cfbac89ff3188fd4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-09 16:54:38 +0530

    Linked List

--- LL/allFunction.java
+++ LL/allFunction.java
@@ -175,8 +175,9 @@ public class allFunction {
                     "Press 1: To Create List\nPress 2: Top Display List\nPress 3: To Insert value in the beginning\nPress 4: To Insert value in the Middle of the List\nPress 5: To Insert the Value in the Last of the List\nPress 6: To Delete the value from the Beginning\nPress 7: To Delete the value from the any Position of the List\nPress 8: To Delete the Value form the Last of the List");
 
             while (true) {
-                int choose = sc.nextInt();
-                sc.nextLine();
+                System.out.println("Press Stop To stop Switching");
+                String choose = sc.nextLine();
+                // sc.nextLine();
                 switch (choose) {
                     case 1:
                         create();

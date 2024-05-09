3c319812509c4c06e8f41596db800862034cb546
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-09 16:57:37 +0530

    Linked List

--- LL/allFunction.java
+++ LL/allFunction.java
@@ -170,7 +170,7 @@ public class allFunction {
             tail = temp;
         }
 
-        public static int press() {
+        public static void press() {
             System.out.println(
                     "Press 1: To Create List\nPress 2: Top Display List\nPress 3: To Insert value in the beginning\nPress 4: To Insert value in the Middle of the List\nPress 5: To Insert the Value in the Last of the List\nPress 6: To Delete the value from the Beginning\nPress 7: To Delete the value from the any Position of the List\nPress 8: To Delete the Value form the Last of the List");
 
@@ -182,7 +182,7 @@ public class allFunction {
                     break;
                 } else {
                     int state = Integer.parseInt(choose);
-                    switch (choose) {
+                    switch (state) {
                         case 1:
                             create();
                             disp();
